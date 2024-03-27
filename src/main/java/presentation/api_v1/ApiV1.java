54da6028a4648bdd5138ef9f54d7b5210b0ea4e1
AR-Life <ayunus155@gmail.com>
2024-03-27 22:58:34 +0300

    initial commit

---
+++ src/main/java/presentation/api_v1/ApiV1.java
@@ -0,0 +1,13 @@
+package presentation.api_v1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiV1 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiV1.class, args);
+    }
+
+}
